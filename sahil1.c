40dc177b89c78f1ee57454c8333d4762df361751
umaretiyasahil58 <sahilumaretiya58@gmail.com>
2024-08-15 14:46:48 +0530

    c - day - 5 sahil

--- sahil1.c
+++ sahil1.c
@@ -2,5 +2,16 @@
 
 int main()
 {
-    g
+    int x,y,ans;
+    // (x+y)2 = (x*x)-2xy-(y*y)
+
+    printf("Enter the value of x = ");
+    scanf("%d",&x);
+    printf("Enter the value of y = ");
+    scanf("%d",&y);
+
+    ans = (x*x)-2*x*y-(y*y);
+    
+    printf("ANSWER = %d",ans);
+    
 }
